68f232392a0a68c4ec9117ec75a782df43c2fa97
Lollypoplove08 <rafferty.smith@hotmail.com>
2021-07-03 13:35:33 +0200

    Update

--- src/main/java/com/santapexie/addon/templates/TorchTemplate.java
+++ src/main/java/com/santapexie/addon/templates/TorchTemplate.java
@@ -2,9 +2,44 @@ package com.santapexie.addon.templates;
 
 import net.minecraft.block.*;
 import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
 
-public class TorchTemplate extends TorchBlock {
-    public TorchTemplate(Settings settings, ParticleEffect particle) {
-        super(settings, particle);
+import java.util.Random;
+
+public class TorchTemplate extends Block {
+    protected static final int field_31265 = 2;
+    protected static final VoxelShape BOUNDING_SHAPE = Block.createCuboidShape(6.0D, 0.0D, 6.0D, 10.0D, 10.0D, 10.0D);
+    protected final ParticleEffect particle;
+
+    public TorchTemplate(AbstractBlock.Settings settings, ParticleEffect particle) {
+        super(settings);
+        this.particle = particle;
+    }
+
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return BOUNDING_SHAPE;
+    }
+
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        return direction == Direction.DOWN && !this.canPlaceAt(state, world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        return sideCoversSmallSquare(world, pos.down(), Direction.UP);
+    }
+
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        double d = (double)pos.getX() + 0.5D;
+        double e = (double)pos.getY() + 0.7D;
+        double f = (double)pos.getZ() + 0.5D;
+        world.addParticle(ParticleTypes.SMOKE, d, e, f, 0.0D, 0.0D, 0.0D);
+        world.addParticle(this.particle, d, e, f, 0.0D, 0.0D, 0.0D);
     }
 }
